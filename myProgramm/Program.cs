060a334ff354d67fed1ea28ad2ae9f2a2c294833
[Alexey] <[lozhkin023@gmail.com]>
2022-11-25 11:24:25 +0300

    done finalwork

--- myProgramm/Program.cs
+++ myProgramm/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("ВВедите текст: ");
+string[] array = Console.ReadLine().Split();
+string[] array2 = new string[array.Length];
+NewArray(array, array2);
+Console.WriteLine();
+Console.WriteLine("массив из строк длинна которых меньше либо ровна 3 символа: ");
+PrintArray(array2);
